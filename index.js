8fb48d4b3a34e49c9cb29e33460b75c863dd99c0
Sagar310 <bhosalesagar310@gmail.com>
2025-01-09 08:58:20 +0530

    Chore: Added Port

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.delete("/hotels/:hotelId", async (req,res) => {
     }
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
-    console.log("Server is running on port", PORT);
+  console.log(`Server is running on port ${PORT}`);
 });
